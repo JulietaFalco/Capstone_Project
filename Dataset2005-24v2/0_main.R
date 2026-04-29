7aea8906751ceef3f8cbd27854a5c8bdd9d102fb
JulietaFalco <julietafalcoar@gmail.com>
2026-04-30 07:40:54 +0930

    Yield data and modelling

--- Dataset2005-24v2/0_main.R
+++ Dataset2005-24v2/0_main.R
@@ -7,5 +7,5 @@ setwd("C:/Users/Usuario/mi-proyecto-ml/Dataset2005-24v2")
 
 source("01_load_data.R")
 source("02_clean_data.R")
-#source("03_correlation.R")
+source("03_correlation.R")
 #source("04_linear_reg.R")
